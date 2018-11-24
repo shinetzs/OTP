6ea954966e9a3de07459dbfedf128d19c24c4ae7
Shinetzs <shinetzs@gmail.com>
2018-11-23 21:26:32 -0300

    a

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 
 
-//Route::get('/', function () {return view('welcome');});
+Route::get('/', function () {return view('main/admin_login');});
 
 //Route::get('/home', 'HomeController@index')->name('home');
 
